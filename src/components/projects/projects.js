706c67c9efbb7ceab5a672b5228209628c9e49e4
Bhanu_Nelluri <bhanunelluri0829@gmail.com>
2021-07-10 18:46:16 +0530

    Stackoverflow Added

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,6 +4,7 @@ import { Link } from 'react-scroll';
 import { HiChevronDoubleUp } from 'react-icons/hi'
 import MemoriesProject from '../../images/MemoriesProject.png';
 import yelpcamp from '../../images/yelpcamp.png';
+import stackoverflow from '../../images/stackoverflow.png'
 
 export default function Projects() {
     return (
@@ -20,6 +21,21 @@ export default function Projects() {
                     className="projectNavBtn"><HiChevronDoubleUp /></Link>
             </div>
             <div className="projectsModule">
+                <div className="project1">
+                    <div className="projectInfo">
+                        <div className="InfoHeader">IITG Stack overflow</div>
+                        <div className="matter">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nostrum sit nemo, obcaecati natus voluptates dolores ex molestiae libero modi! Ducimus laudantium nemo modi explicabo assumenda natus rem dolorum quam.lorem
+                        </div>
+                    </div>
+                    <div className="projectImage">
+                        <img className="image" src={stackoverflow} alt="StackOverCloned"></img>
+                    </div>
+                </div>
+                <div className="projectFooter">
+                    <a href="https://stack-over-cloned.netlify.app" className="visit">VISIT</a>
+                    <a href="https://github.com/BhanuNelluri/StackOverCloned" className="github">GITHUB</a>
+                </div>
                 <div className="project1">
                     <div className="projectInfo">
                         <div className="InfoHeader">MemoriesProject</div>
@@ -46,25 +62,11 @@ export default function Projects() {
                         <img className="image" src={yelpcamp} alt="YelpCamp"></img>
                     </div>
                 </div>
-                <div className="projectFooter">
+                <div className="projectFooter1">
                     <a href="https://yelpcamp-application02.herokuapp.com/" className="visit">VISIT</a>
                     <a href="https://github.com/BhanuNelluri/YelpCamp_Project" className="github">GITHUB</a>
                 </div>
-                <div className="project1">
-                    <div className="projectInfo">
-                        <div className="InfoHeader">IITG Stack overflow</div>
-                        <div className="matter">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nostrum sit nemo, obcaecati natus voluptates dolores ex molestiae libero modi! Ducimus laudantium nemo modi explicabo assumenda natus rem dolorum quam.lorem
-                        </div>
-                    </div>
-                    <div className="projectImage">
-                        <h2>Comming Soon!</h2>
-                    </div>
-                </div>
-                <div className="projectFooter1">
-                    <a className="visit">VISIT</a>
-                    <a className="github">GITHUB</a>
-                </div>
+
             </div>
         </div>
     )
